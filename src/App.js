a2abfffe95069a111230b14c8791298b4a14374c
matt.farmer <mrfarmer777@gmail.com>
2023-03-19 10:09:56 -0500

    Remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import NotationDisplay from "./components/NotationDisplay";
-import FunctionButton from "./components/FunctionButton";
 import { useState } from 'react'
 import NoteChooser from "./components/NoteChooser";
 import NoteNameButtons from "./components/NoteNameButtons";
@@ -27,7 +26,6 @@ const theme = {
 
 function App() {
   const [note, setNote] = useState({noteName: "c", octave: "4"});
-  // const [guess, setGuess] = useState('');
   const [level, setLevel] = useState('note-names');
   const [numCorrect, setNumCorrect] = useState(0)
   const [numAttempts, setNumAttempts] = useState(0)
