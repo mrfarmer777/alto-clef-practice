63d8d10944415b0a4895160bfd4863f7a785df41
matt.farmer <mrfarmer777@gmail.com>
2023-03-19 10:05:33 -0500

    Add fingerboard button implementation and toggle for input types

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import FunctionButton from "./components/FunctionButton";
 import { useState } from 'react'
 import NoteChooser from "./components/NoteChooser";
 import NoteNameButtons from "./components/NoteNameButtons";
-import { Grommet, Page, PageContent, Box, Button } from 'grommet';
+import FingerboardButtons from "./components/FingerboardButtons";
+import { Grommet, Page, PageContent, Box, Button, RadioButtonGroup } from 'grommet';
 
 const theme = {
   global: {
@@ -26,16 +27,24 @@ const theme = {
 
 function App() {
   const [note, setNote] = useState({noteName: "c", octave: "4"});
-  const [guess, setGuess] = useState('');
+  // const [guess, setGuess] = useState('');
+  const [level, setLevel] = useState('note-names');
   const [numCorrect, setNumCorrect] = useState(0)
   const [numAttempts, setNumAttempts] = useState(0)
 
   const checkGuess = function (e) {
-    if(e.target.value === note["noteName"]){
-      setNumCorrect(numCorrect + 1)
+    const guessedNote = e.target.value[0]
+    const guessedOctave = e.target.value[1]
+    if(guessedOctave) {
+      if (guessedOctave === note["octave"] && guessedNote === note["noteName"]) {
+        setNumCorrect(numCorrect + 1)
+      }
+    } else {
+      if(guessedNote === note["noteName"]){
+        setNumCorrect(numCorrect + 1)
+      }
     }
     setNumAttempts(numAttempts + 1)
-    setGuess('')
     selectNewNote()
   }
 
@@ -51,6 +60,15 @@ function App() {
           <Box direction='column' justify='center'>
             <Box direction='row' justify='center'>
               <h3 data-testid={'score-display'}>{ `Score: ${numCorrect}/${numAttempts}` }</h3>
+
+            </Box>
+            <Box direction='row' justify='center'>
+              <RadioButtonGroup
+                name="doc"
+                options={['note-names', 'fingerboard-positions']}
+                value={level}
+                onChange={(event) => setLevel(event.target.value)}
+              />
             </Box>
             <Box direction='row' justify='center'>
               <NotationDisplay targetNote={note["noteName"]} octave={note["octave"]}/>
@@ -61,7 +79,10 @@ function App() {
             <Box direction='column' width='small'>
               <Button label='New Note' fill='vertical' onClick={selectNewNote}/>
             </Box>
-            <NoteNameButtons checkNote={checkGuess} />
+            { level === 'note-names' ?
+              <NoteNameButtons checkNote={checkGuess} /> :
+              <FingerboardButtons checkNote={checkGuess} />
+            }
           </Box>
         </PageContent>
       </Page>
